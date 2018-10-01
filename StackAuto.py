a3a3a658c19d99a30fbaa5233b5093a4ca52e057
everton-zoio <evertonabreu@alunos.utfpr.edu.br>
2018-10-01 13:24:38 -0300

    att

--- StackAuto.py
+++ StackAuto.py
@@ -13,7 +13,7 @@ class Pilha():
     ## quantos caracteres irão ser comparados
     return (False) if (len(self._conteudo) < length) else (self._conteudo[0:length] == conteudo)
     ## retorna falso, se conteudo da pilha for menor que o conteudo comparado ou diferente dele
-  
+  ## desempilha valor antigo e empilha o novo independente de ser valor vazio
   def alterTop(self, old, new):
     if (self.isTop(old)):
       self._conteudo = self._conteudo.replace(old, new, 1)
@@ -27,7 +27,7 @@ class Pilha():
 
 class Transicao():
 
-  def __init__(self,transicao, estado, estados):
+  def __init__(self, transicao, estado, estados):
     data = transicao.split()
     self.estado = estado
     self.novoEstado = data[1]
@@ -71,14 +71,17 @@ def lerArquivo (arquivo):
   #print(conjEstados)
   iniEstado = conteudo[5].split()
   #print(iniEstado)
+  finEstados = conteudo[6].split()
+  #print(finEstados)
   transicoes = conteudo[7:]
   #print(transicoes)
   arq.close()
+  return [alfaEntrada, alfaPilha, conjEstados, iniEstado, finEstados, transicoes]
 
 def main(arquivo, entrada):
 
   estrutura = lerArquivo(arquivo)
-
+  print(estrutura[0])
 
   ## Testes da pilha
   pil = Pilha('ola mundo')
@@ -94,3 +97,24 @@ if __name__ == "__main__":
     print("Parametros insuficientes. Informe o nome do arquivo de entrada e a palavra de entrada")
     sys.exit(1)
   main(sys.argv[1], sys.argv[2])
+
+
+
+
+
+
+### na transição 
+  
+#  def valida(self, fita, pilha):
+#    if (fita.isTop(self.tapeContent)): ## valida a fita
+#      ## esqueci de declarar o valor da fita na transição
+#      if (pilha.isTop(self.valor)): ## valida a pilha
+#        return True
+#    ## retorna falso se a fita ou a pilha estiver invalida
+#    return False
+
+### na maquina 
+#  def __init__(self, entrada, pilha, estado):
+#    self.fita = Pilha(entrada)
+#    self.pilha = Pilha(pilha)
+#    self.estado = estado
